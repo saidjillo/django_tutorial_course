aeecbb39f7d24aff5c1e99bad473c366fd47c3ae
saidjillo <umurjillo@gmail.com>
2018-11-14 12:13:54 +0300

    Initial commit

---
+++ shop/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from django.urls import path, include
+
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('games', views.get_games, name='games'),
+    path('html', views.get_html, name='html'),
+]
