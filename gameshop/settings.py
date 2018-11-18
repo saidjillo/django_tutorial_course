7c8489b3489c9e26e0f9dc6e2d759c3a3f94d82e
saidjillo <umurjillo@gmail.com>
2018-11-18 04:45:27 +0300

    modified settings.py

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 STATIC_URL = '/static/'
