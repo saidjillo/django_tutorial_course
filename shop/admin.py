f459d07f120a2fe47bc1ea8f895c9f014610f830
saidjillo <umurjillo@gmail.com>
2018-11-18 05:43:44 +0300

    modified home.html

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from shop.models import Game , Player, Developer, Transaction
 
-Register your models here.
+# Register your models here.
 admin.site.register(Game)
 admin.site.register(Player)
 admin.site.register(Developer)
