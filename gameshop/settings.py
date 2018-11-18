912dd108415b3904d19d46ee9d3ca814131cebbe
saidjillo <umurjillo@gmail.com>
2018-11-18 04:40:47 +0300

    modified settings.py

--- gameshop/settings.py
+++ gameshop/settings.py
@@ -86,12 +86,12 @@ AUTHENTICATION_BACKENDS = (
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
+ DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# }
+ }
 DATABASES['default'] = dj_database_url.config()
 
 
